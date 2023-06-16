999193aa5f88e26189b134e75e373b37a47dd8dc
Melodica4585 <melodica4585@gmail.com>
2023-06-16 19:41:18 +0300

    changed APIversion to 2.5

--- src/index.js
+++ src/index.js
@@ -106,7 +106,7 @@ function displayForecast(response) {
 function getForecast(coordinates) {
     console.log(coordinates);
     let apiKey = "e0b94a510e1371db880f85653a8e84fe";
-    let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&unit=metric`
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&unites=${units}`;
     axios.get(apiUrl).then(displayForecast);
 }
 
